0fe395c47a6272c2ce406644efb9c2b39cc74103
Juraj Michalek <juraj.michalek@gmail.com>
2023-11-28 17:33:53 +0100

    add second mode of candle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 #![no_std]
 #![no_main]
 
-use hal::{clock::ClockControl, peripherals, prelude::*, rmt::Rmt, Delay, IO,
-clock::CpuClock};
+use hal::{
+    clock::ClockControl,
+    peripherals,
+    prelude::*,
+    rmt::Rmt,
+    rng::Rng,
+    Delay,
+    IO,
+    clock::CpuClock
+};
 use esp_backtrace as _;
 use esp_hal_smartled::{smartLedAdapter, SmartLedsAdapter};
 use smart_leds::{
@@ -12,6 +20,14 @@ use smart_leds::{
     SmartLedsWrite,
 };
 
+// Modes
+enum Mode {
+    Rainbow,
+    Candle,
+    Flame,
+}
+
+
 #[entry]
 fn main() -> ! {
     let peripherals = peripherals::Peripherals::take();
@@ -19,6 +35,10 @@ fn main() -> ! {
     let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock80MHz).freeze();
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let button = io.pins.gpio9.into_pull_up_input();
+    let mut rng = Rng::new(peripherals.RNG);
+    let mut current_mode = Mode::Rainbow;
 
     let rmt = Rmt::new(peripherals.RMT, 80u32.MHz(), &clocks).unwrap();
 
@@ -36,23 +56,84 @@ fn main() -> ! {
         val: 255,
     };
 
+    let flame_color = Hsv {
+        hue: 25,
+        sat: 240,
+        val: 180,
+    };
+
+
     let mut data;
     let mut data_strip;
+    let mut rainbow_hue = 0;
 
     loop {
-        // Iterate over the rainbow!
-        for hue in 0..=255 {
-            color.hue = hue;
-            let value = hsv2rgb(color);
-            data = [value];
-            data_strip = [value, value, value, value];
+        // Check button press to change mode
+        if button.is_low().unwrap() {
+            current_mode = match current_mode {
+                Mode::Rainbow => Mode::Candle,
+                Mode::Candle => Mode::Flame,
+                Mode::Flame => Mode::Rainbow,
+            };
+            // Debouncing delay
+            delay.delay_ms(1000u16);
+        }
 
-            led.write(brightness(gamma(data.iter().cloned()), 60))
-            .unwrap();
+        match current_mode {
+            Mode::Rainbow => {
+                // Iterate over the rainbow!
+                rainbow_hue = rainbow_hue + 1;
+                if rainbow_hue > 254 {
+                    rainbow_hue = 0;
+                }
+                color.hue = rainbow_hue;
+                let value = hsv2rgb(color);
+                data = [value];
+                data_strip = [value, value, value, value];
 
-            led_strip.write(brightness(gamma(data_strip.iter().cloned()), 60))
+                led.write(brightness(gamma(data.iter().cloned()), 60))
                 .unwrap();
-            delay.delay_ms(250u8);
+
+                led_strip.write(brightness(gamma(data_strip.iter().cloned()), 60))
+                    .unwrap();
+                delay.delay_ms(250u8);
+            },
+            Mode::Candle => {
+                // Base brightness and flicker mask
+                let base_brightness: u8 = 180; // Adjust as needed
+                let flicker_mask: u8 = 0x1F; // Mask for lower 5 bits
+
+                // Generate a flicker within the range using bitmask
+                let mut buf = [0u8; 1];
+                rng.read(&mut buf).unwrap();
+                let flicker = (buf[0] & flicker_mask).wrapping_sub(16); // Use wrapping_sub for overflow handling
+
+                // Calculate new brightness with overflow handling
+                let new_brightness = base_brightness.wrapping_add(flicker);
+
+                // Apply the new brightness
+                let color = hsv2rgb(Hsv {
+                    hue: flame_color.hue,
+                    sat: 255,
+                    val: new_brightness,
+                });
+                let data = [color];
+                let data_strip = [color, color, color, color];
+
+                led.write(brightness(gamma(data.iter().cloned()), 60))
+                    .unwrap();
+
+                led_strip.write(brightness(gamma(data_strip.iter().cloned()), 255))
+                    .unwrap();
+
+                // Randomized delay
+                rng.read(&mut buf).unwrap();
+                let delay_time = (buf[0] & 0x3F) + 50;
+                delay.delay_ms(delay_time as u8);
+            },
+
+            Mode::Flame => {
+            }
         }
     }
 }
